16a72109e5eb373cea4064d7e60d02e31d7759c3
c220622c <wzj50329@gmail.com>
2026-01-22 17:15:19 +0800

    everything that makes me whole~

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -43,41 +43,180 @@ return {
 		},
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
-			documentation = { auto_show = true },
+			documentation = { -- Controls whether the documentation window will automatically show when selecting a completion item
+				auto_show = true,
+				-- Delay before showing the documentation window
+				auto_show_delay_ms = 500,
+				-- Delay before updating the documentation window when selecting a new item,
+				-- while an existing item is still visible
+				update_delay_ms = 50,
+				-- Whether to use treesitter highlighting, disable if you run into performance issues
+				treesitter_highlighting = true,
+				-- Draws the item in the documentation window, by default using an internal treesitter based implementation
+				draw = function(opts)
+					opts.default_implementation()
+				end,
+				window = {
+					min_width = 25,
+					max_width = 80,
+					max_height = 20,
+					border = "rounded", -- Defaults to `vim.o.winborder` on nvim 0.11+ or 'padded' when not defined/<=0.10
+					winblend = 0,
+					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+					-- Note that the gutter will be disabled when border ~= 'none'
+					scrollbar = true,
+					-- Which directions to show the documentation window,
+					-- for each of the possible menu window directions,
+					-- falling back to the next direction when there's not enough space
+					direction_priority = {
+						menu_north = { "e", "w", "n", "s" },
+						menu_south = { "e", "w", "s", "n" },
+					},
+				},
+			},
 			list = { selection = { preselect = false, auto_insert = false } },
 			keyword = { range = "full" },
 			-- Disable auto brackets
 			-- NOTE: some LSPs may add auto brackets themselves anyway
 			accept = { auto_brackets = { enabled = false } },
+			-- Display a preview of the selected item on the current line
+			ghost_text = { enabled = true },
 			menu = {
-				-- Don't automatically show the completion menu
+				enabled = true,
+				min_width = 17,
+				max_height = 10,
+				border = "rounded", -- Defaults to `vim.o.winborder` on nvim 0.11+
+				winblend = 0,
+				winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+				-- Keep the cursor X lines away from the top/bottom of the window
+				scrolloff = 2,
+				-- Note that the gutter will be disabled when border ~= 'none'
+				scrollbar = true,
+				-- Which directions to show the window,
+				-- falling back to the next direction when there's not enough space
+				direction_priority = { "s", "n" },
+				-- Can accept a function if you need more control
+				-- direction_priority = function()
+				--   if condition then return { 'n', 's' } end
+				--   return { 's', 'n' }
+				-- end,
+
+				-- Whether to automatically show the window when new completion items are available
 				auto_show = true,
+				-- Delay before showing the completion menu
+				auto_show_delay_ms = 0,
 
-				-- nvim-cmp style menu
+				-- Screen coordinates of the command line
+				cmdline_position = function()
+					if vim.g.ui_cmdline_pos ~= nil then
+						local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+						return { pos[1] - 1, pos[2] }
+					end
+					local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+					return { vim.o.lines - height, 0 }
+				end,
 				draw = {
+					-- Aligns the keyword you've typed to a component in the menu
+					align_to = "label", -- or 'none' to disable, or 'cursor' to align to the cursor
+					-- Left and right padding, optionally { left, right } for different padding on each side
+					padding = 1,
+					-- Gap between columns
+					gap = 1,
+					-- Priority of the cursorline highlight, setting this to 0 will render it below other highlights
+					cursorline_priority = 10000,
+					-- Appends an indicator to snippets label
+					snippet_indicator = "~",
+					-- Use treesitter to highlight the label text for the given list of sources
+					treesitter = {},
+					-- treesitter = { 'lsp' }
 
-					kind_icon = {
-						text = function(ctx)
-							local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-							return kind_icon
-						end,
-						-- (optional) use highlights from mini.icons
-						highlight = function(ctx)
-							local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-							return hl
-						end,
-					},
-					kind = {
-						-- (optional) use highlights from mini.icons
-						highlight = function(ctx)
-							local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-							return hl
-						end,
+					-- Components to render, grouped by column
+					columns = { { "kind_icon" }, { "label", "label_description", gap = 1 } },
+
+					-- Definitions for possible components to render. Each defines:
+					--   ellipsis: whether to add an ellipsis when truncating the text
+					--   width: control the min, max and fill behavior of the component
+					--   text function: will be called for each item
+					--   highlight function: will be called only when the line appears on screen
+					components = {
+						kind_icon = {
+							ellipsis = false,
+							text = function(ctx)
+								return ctx.kind_icon .. ctx.icon_gap
+							end,
+							-- Set the highlight priority to 20000 to beat the cursorline's default priority of 10000
+							highlight = function(ctx)
+								return { { group = ctx.kind_hl, priority = 20000 } }
+							end,
+						},
+
+						kind = {
+							ellipsis = false,
+							width = { fill = true },
+							text = function(ctx)
+								return ctx.kind
+							end,
+							highlight = function(ctx)
+								return ctx.kind_hl
+							end,
+						},
+
+						label = {
+							width = { fill = true, max = 60 },
+							text = function(ctx)
+								return ctx.label .. ctx.label_detail
+							end,
+							highlight = function(ctx)
+								-- label and label details
+								local highlights = {
+									{
+										0,
+										#ctx.label,
+										group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+									},
+								}
+								if ctx.label_detail then
+									table.insert(
+										highlights,
+										{ #ctx.label, #ctx.label + #ctx.label_detail, group = "BlinkCmpLabelDetail" }
+									)
+								end
+
+								-- characters matched on the label by the fuzzy matcher
+								for _, idx in ipairs(ctx.label_matched_indices) do
+									table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+								end
+
+								return highlights
+							end,
+						},
+
+						label_description = {
+							width = { max = 30 },
+							text = function(ctx)
+								return ctx.label_description
+							end,
+							highlight = "BlinkCmpLabelDescription",
+						},
+
+						source_name = {
+							width = { max = 30 },
+							text = function(ctx)
+								return ctx.source_name
+							end,
+							highlight = "BlinkCmpSource",
+						},
+
+						source_id = {
+							width = { max = 30 },
+							text = function(ctx)
+								return ctx.source_id
+							end,
+							highlight = "BlinkCmpSource",
+						},
 					},
 				},
 			},
-			-- Display a preview of the selected item on the current line
-			ghost_text = { enabled = true },
 		},
 		-- Controls how the completion items are rendered on the popup window
 		-- Default list of enabled providers defined so that you can extend it
